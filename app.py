1047d82cb527ad5f89836fc47b51355d43ea5aec
SVinayak35 <singhvinayak35@gmail.com>
2025-06-06 10:32:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def count_people():
     return jsonify({'person_count': count})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
